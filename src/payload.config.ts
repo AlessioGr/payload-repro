c238ecf55be7a8673820c1ec63654d342a3019b6
Alessio Gravili <alessio@gravili.de>
2022-12-04 14:59:47 +0100

    simplify

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,31 +7,11 @@ import Users from './collections/Users';
 import Pages from "./collections/Pages";
 import Media from "./collections/Media";
 
-import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
-import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
 
-const adapter = s3Adapter({
-  config: {
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
-    endpoint: process.env.S3_ENDPOINT,
-    region: 'auto',
-  },
-  bucket: process.env.S3_BUCKET,
-})
 
 export default buildConfig({
   serverURL: process.env.SERVER_URL,
   plugins: [
-    cloudStorage({
-      collections: {
-        'media': {
-          adapter: adapter, // see docs for the adapter you want to use
-        },
-      },
-    }),
   ],
   admin: {
     user: Users.slug,
